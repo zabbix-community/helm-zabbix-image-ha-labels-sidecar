dc4740d1d51aca74eb2854e709c00e0279b77bd9
Christian Anton <christian.anton@inqbeo.de>
2024-11-17 20:00:05 +0100

    implemented a logging function

--- ha-label-manager.sh
+++ ha-label-manager.sh
@@ -3,14 +3,22 @@
 # we assume our pod is called like the hostname, which is default
 pod_name=${HOSTNAME}
 port_number=${ZBX_LISTENPORT:-10051}
-label_name="zabbix.com/server-ha-role"
+label_name=${LABEL_NAME}
 sleep_time=5
+
+log() {
+    msg=$1
+    ts=$(date +%"%Y-%m-%d %h:%m:%s")
+    echo "${ts} ${msg}"
+}
+
+log "start label manager with label name ${label_name}"
 
 update_label() {
     current_value=$1
     new_value=$2
     if [ "${current_value}" != "${new_value}" ]; then
-        echo "setting label ${label_name} from value ${current_value} to ${new_value}"
+        log "setting label ${label_name} from value ${current_value} to ${new_value}"
         kubectl label pod ${pod_name} "${label_name}"="${new_value}" --overwrite
     fi
 }
@@ -21,16 +29,16 @@ do
     if [[ -z "$last_label_read" || $((now - last_label_read)) -gt 300 ]]; then
         label_value=$(kubectl get pod ${pod_name} -o json | jq -r ".metadata.labels[\"${label_name}\"]")
         last_label_read=${now}
-        echo "got label value ${label_value}"
+        log "got label value ${label_value}"
     fi
 
     # check whether listen-port is open
     if { echo > /dev/tcp/localhost/${port_number}; } 2>/dev/null; then
-        echo "port ${port_number} is open"
+        log "port ${port_number} is open"
         update_label "${label_value}" "active"
 	label_value="active"
     else
-        echo "port ${port_number} is closed"
+        log "port ${port_number} is closed"
         update_label "${label_value}" "standby"
 	label_value="standby"
     fi
